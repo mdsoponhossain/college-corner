c5977d202ee4e3f5e5e40441528e08a0cc8976db
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2024-07-10 12:18:01 +0600

    footer is updated

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,36 +1,60 @@
-
+import { TfiEmail } from "react-icons/tfi";
+import { FaHome, FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineFax } from "react-icons/md";
+import { useContext } from "react";
+import { AuthContext } from "../../contextProvider/ContextProvider";
 const Footer = () => {
   // bg-[rgb(20,69,47)] text-white
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div className="bg-[rgb(20,69,47)] py-3 md:py-4 lg:py-8">
+    <div className="bg-[rgb(20,69,47)] py-4 md:py-5 lg:py-9">
       <footer className="footer   text-white pt-10 px-3 max-w-[1280px] mx-auto">
         <aside>
           <img className="w-20 h-20" src="https://i.ibb.co/Fw5wkkX/college-Corner.png" alt="" />
           <p>
             CollegeCorner.edu
             <br />
-            Providing reliable tech since 1992
+            Providing educational services since 1992
           </p>
         </aside>
+
         <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <h6 className="footer-title">Useful Links</h6>
+          <a href='/' className="link link-hover">Home</a>
+          <a href="/colleges" className="link link-hover">Colleges</a>
+          {
+            !currentUser && <a href="/login" className="link link-hover">Login</a>
+          }
+          {
+            !currentUser && <a href="/sign-up" className="link link-hover">Sign Up</a>
+          }
+          {
+            currentUser?.email && <a href="/admission" className="link link-hover">Admisssion</a>
+          }
+          {
+            currentUser?.email && <a href={`/my-college/${currentUser?.email}`} className="link link-hover">My College</a>
+          }
         </nav>
         <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">Home</a>
-          <a className="link link-hover">Colleges</a>
-          <a className="link link-hover">Admisssion</a>
-          <a className="link link-hover">My College</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <h6 className="footer-title">Contact</h6>
+          <a className="link link-hover flex items-center gap-3">
+            <FaHome className="text-xl font-bold"></FaHome>
+            <span>Melbourne,Austrilia</span>
+          </a>
+          <a className="link link-hover flex items-center gap-3">
+            <TfiEmail className="text-xl font-bold"></TfiEmail>
+            <span>college@corner.gmail.com</span>
+          </a>
+          <a className="link link-hover flex items-center gap-3">
+            <FaPhoneAlt className="text-xl font-bold"></FaPhoneAlt>
+            <span>+61 36749236</span>
+          </a>
+          <a className="link link-hover flex items-center gap-3">
+            <MdOutlineFax className="text-xl font-bold"></MdOutlineFax>
+            <span>+61 464783436</span>
+          </a>
+
+
         </nav>
       </footer>
     </div>
